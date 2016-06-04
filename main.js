bb284aaef77ceb7db4d8fb9b1bd9220d4e5de027
Guillaume Wuip <guillaume@wuips.com>
2016-06-04 08:32:26 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,39 @@
+(() => {
+
+    'use strict';
+
+    const
+        Botkit = require('botkit');
+
+    const
+        ACCESS_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN,
+        VERIFY_TOKEN = process.env.FACEBOOK_VERIFY_TOKEN,
+        PORT         = process.env.PORT || 8080;
+
+    if (!ACCESS_TOKEN) {
+        throw new Error('FACEBOOK_PAGE_ACCESS_TOKEN is required but missing');
+    }
+
+    if (!VERIFY_TOKEN) {
+        throw new Error('FACEBOOK_VERIFY_TOKEN is required but missing');
+    }
+
+    const controller = Botkit.facebook({
+        access_token: ACCESS_TOKEN,
+        verify_token: VERIFY_TOKEN
+    });
+
+    const bot = controller.spawn();
+
+    controller.setupWebServer(PORT, (err, webserver) => {
+
+        if (err) {
+            console.error(err);
+        }
+
+        controller.createWebHookEndpoints(webserver, bot, () => {
+            console.log(`Listening on port ${PORT}`);
+        });
+    });
+
+}());
